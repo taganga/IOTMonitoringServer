ddd88cdf8e8810c6564601cbaf960bc172fbc2ee
taganga <jc.pupo@uniandes.edu.co>
2023-09-19 18:38:13 -0500

    modificacion de IPS

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.234.169.201"]
+ALLOWED_HOSTS = ["localhost", "54.242.82.89"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.89.249.231",  # Dirección IP de la base de datos
+        "HOST": "3.89.30.102",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.85.212.228"
+MQTT_HOST = "52.91.140.208"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
